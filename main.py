b51014434b5b95e683104fb0fe9a65f4850d1d7d
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-03-14 14:41:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+
 print('Hello I am the Fots virtual assistant')
 q = input('How can I help you?')
-print('I will try to help with', q)
+print('I will try to help with that')
 also = input('Is there anything else you need because Im going to take a break soon?')
 print('Im sorry the Fots Virtual Assistant does not know how to do that. TRY A RISPE FOT AND MAYBE YOULL FEE BETTER.')
+rating = input('How did you like the Fots Virtual Assistant? One out of five')
+print('Thank you for rating me a', rating)
+print('Happy fotting!')
