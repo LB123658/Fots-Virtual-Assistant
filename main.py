2053fc58e94618cc5fb6e5aa25425a4f5e28eacd
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-03-15 11:51:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
-print('Hello I am the Fots virtual assistant')
+name = input('Enter your name:')
+print('I am the Fots virtual assistant. Hello',name)
 q = input('How can I help you?')
 print('I will try to help with that')
 also = input('Is there anything else you need because Im going to take a break soon?')
@@ -7,3 +7,7 @@ print('Im sorry the Fots Virtual Assistant does not know how to do that. TRY A R
 rating = input('How did you like the Fots Virtual Assistant? One out of five')
 print('Thank you for rating me a', rating)
 print('Happy fotting!')
+did = input('If this didnt help please describe your issue:')
+print('I see you need help with:',did)
+website = input('Which FOTS website are you using?')
+print('I recomment you go to https://lb123658.github.io/Fots/ for a better experience. Contact Fots at fotsofficial671@gmail.com')
